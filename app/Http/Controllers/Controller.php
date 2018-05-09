493bd4f185206e6b036a75528ff31f50e7535d01
ZongzhiCui <20768272@qq.com>
2018-05-09 11:34:37 +0800

    缩略图问题修改完成..在服务端需要建立软连接

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,10 +16,16 @@ class Controller extends BaseController
     //上传图片,制作缩略图
     public function thumb($filename,$width=100,$height=100)
     {
-        $path_parts = pathinfo(Storage::url($filename)); //Storage::url($filename);这个才是可用的图片路径
+        $filename = public_path().storage::url($filename);
+        //dd($filename);
+        $path_parts = pathinfo($filename); //storage::url($filename);这个才是可用的图片路径
+        //dd($path_parts);//public_path().Storage::url($filename);
         $i_mg = $path_parts['filename'].'_'.$width.'X'.$height.'.'.$path_parts['extension']; //拼接缩略图文件路径
-        $img = Image::make(public_path().Storage::url($filename))->resize($width, $height);//图片资源必须绝对路径!缩略图
-        $img->save(public_path().$path_parts['dirname'].'/'.$i_mg);
+        //dd($i_mg);
+        $img = Image::make($filename)->resize($width, $height);//图片资源必须绝对路径!缩略图
+        //dd($img);
+        $img->save($path_parts['dirname'].'/'.$i_mg);
+        //dd(dirname($filename).'/'.$i_mg);
         return dirname($filename).'/'.$i_mg;
     }
     //上传到阿里OSS
